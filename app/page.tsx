ba1e128457150f89a7732caf0706593ac809d3ef
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-18 14:19:20 -0300

    home page with projects list

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
@@ -6,10 +8,45 @@ export default async function Home() {
   console.log(projects);
 
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div className="max-w-5xl mx-auto text">
+      <h1>
+        Hi there, I&apos;m{" "}
+        <span className="bg-gradient-to-r from-blue-400 via-purple-800 to-blue-600 bg-clip-text text-transparent">
+          Bruno
+        </span>
+        !
+      </h1>
+
+      <p className="mt-3">
+        Glad to have you over! Check out some of my projects
+      </p>
+
+      <h2 className="mt-24">My Projects</h2>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mt-5">
+        {projects.map((project) => (
+          <Link
+            href={`/projects/${project.slug}`}
+            className="p-3 border-2 border-gray-500 rounded-lg hover:scale-105 hover:border-blue-500 transition duration-300"
+            key={project._id}
+          >
+            {/* Image */}
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.alt}
+                width={500}
+                height={500}
+                className="object-contain w-full h-56 rounded-lg border"
+              />
+            )}
+            {/* name */}
+            <h3 className="bg-gradient-to-r from-blue-400 via-purple-800 to-blue-600 bg-clip-text text-transparent">
+              {project.name}
+            </h3>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
-  plugins: [],
-};
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const darkMode = "class";
+export const theme = {};
+export const plugins = [];
