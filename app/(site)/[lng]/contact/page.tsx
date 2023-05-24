28ecdb26102063210a6617a8a89e06484da66db5
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-24 10:26:32 -0300

    correcting bottom padding on small screens

--- app/(site)/[lng]/contact/page.tsx
+++ app/(site)/[lng]/contact/page.tsx
@@ -33,8 +33,8 @@ const Page = async ({ params: { lng } }: PageProps) => {
     <>
       <Navigation lng={lng} />
 
-      <div className=" container mx-auto flex min-h-screen animate-fade-right items-center justify-center px-4">
-        <div className="mx-auto mt-32 grid w-full grid-cols-1 gap-8 sm:mt-0 sm:grid-cols-3 xl:gap-16">
+      <div className="container mx-auto flex min-h-screen animate-fade-right items-center justify-center px-4 pb-20 md:pb-0">
+        <div className="mx-auto grid w-full grid-cols-1 gap-8 sm:mt-0 lg:grid-cols-3 xl:gap-16">
           {socials.map((s) => (
             <Card key={s.label}>
               <Link
